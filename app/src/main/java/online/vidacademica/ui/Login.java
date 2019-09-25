307994252a29d01581d946944039d89fb1ff6519
Tiago <tiagomnunes@hotmail.com>
2019-09-25 12:49:47 -0300

    start screen login

---
+++ app/src/main/java/online/vidacademica/ui/Login.java
@@ -0,0 +1,16 @@
+package online.vidacademica.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import online.vidacademica.R;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
