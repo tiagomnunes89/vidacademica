9bf2808f447c553ece7c1ba08b0ba6efa17b63a5
Eduardo Augusto <eduardoaugusto2016880@gmail.com>
2019-11-25 13:20:55 -0300

    Alterações Tela Criar Curso

--- app/src/main/java/online/vidacademica/presentation/CreateCourseActivity.java
+++ app/src/main/java/online/vidacademica/presentation/CreateCourseActivity.java
@@ -9,6 +9,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import online.vidacademica.R;
 import online.vidacademica.databinding.ActivityCreateCourseBinding;
@@ -18,6 +21,9 @@ public class CreateCourseActivity extends AppCompatActivity {
 
     private ActivityCreateCourseBinding binding;
 
+    private String[] list_status_course = new String[]{"Selecione...","Ativo","Inativo"};
+    private String status_course;
+    private Spinner sp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +31,31 @@ public class CreateCourseActivity extends AppCompatActivity {
         setContentView(R.layout.activity_create_course);
 
 
+        // Populando Spinner
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item,list_status_course);
+
+        sp = (Spinner) findViewById(R.id.input_status_course);
+        sp.setAdapter(adapter);
+
+
+        sp.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                switch (position){
+                    case 1:
+                        status_course = "Ativo";
+                        break;
+                    case 2:
+                        status_course = "Inativo";
+                        break;
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+                return;
+            }
+        });
 
     }
 
