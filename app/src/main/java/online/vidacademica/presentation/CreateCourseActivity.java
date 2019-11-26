230a05ccb6dd67375aef0021a55cd4d4677bafe9
Eduardo Augusto <eduardoaugusto2016880@gmail.com>
2019-11-26 19:39:07 -0300

    Alert tela de criar curso

--- app/src/main/java/online/vidacademica/presentation/CreateCourseActivity.java
+++ app/src/main/java/online/vidacademica/presentation/CreateCourseActivity.java
@@ -1,29 +1,36 @@
 package online.vidacademica.presentation;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import online.vidacademica.R;
 import online.vidacademica.databinding.ActivityCreateCourseBinding;
 import online.vidacademica.databinding.ActivityCreateSubjectBinding;
+import online.vidacademica.presentation.ui.CreateSubjectActivity;
 
 public class CreateCourseActivity extends AppCompatActivity {
 
     private ActivityCreateCourseBinding binding;
 
-    private String[] list_status_course = new String[]{"Selecione...","Ativo","Inativo"};
+    private String[] list_status_course = new String[]{"Ativo","Inativo"};
     private String status_course;
     private Spinner sp;
+    private Button btnSaveCourse;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,6 +63,38 @@ public class CreateCourseActivity extends AppCompatActivity {
                 return;
             }
         });
+
+
+//        Alert para levar usuario para a tela de cadastro de materia
+            btnSaveCourse = (Button) findViewById(R.id.btn_save_course);
+            btnSaveCourse.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    AlertDialog.Builder  alert = new AlertDialog.Builder(CreateCourseActivity.this);
+                    alert.setTitle("Aviso");
+                    alert
+                            .setIcon(R.drawable.ic_info_black_24dp)
+                            .setMessage("Deseja cadastrar uma matéria?")
+                            .setCancelable(false)
+                            .setNegativeButton("Não", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialogInterface, int i) {
+                                    Toast.makeText(getApplicationContext(),"Cancelar escolhido",Toast.LENGTH_LONG).show();
+                                }
+                            })
+                            .setPositiveButton("Sim", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialogInterface, int i) {
+                                    startActivity(new Intent(CreateCourseActivity.this, CreateSubjectActivity.class));
+                                }
+                            });
+
+                    AlertDialog alertDialog = alert.create();
+                    alertDialog.show();
+
+
+                }
+            });
 
     }
 
