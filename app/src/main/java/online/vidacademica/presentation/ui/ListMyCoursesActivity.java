39654e9645fc1da9339d9e36530dd2b1cd836c0c
Eduardo Augusto <eduardoaugusto2016880@gmail.com>
2019-11-28 09:54:44 -0300

    Tela de meus cursos

--- app/src/main/java/online/vidacademica/presentation/ui/ListMyCoursesActivity.java
+++ app/src/main/java/online/vidacademica/presentation/ui/ListMyCoursesActivity.java
@@ -20,8 +20,7 @@ public class ListMyCoursesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_my_courses);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
