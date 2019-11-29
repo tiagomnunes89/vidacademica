4dfeed152d432a4806e9ac80350e66cbbc55c157
Carlos <cgletras@gmail.com>
2019-11-29 19:03:21 -0300

    list my activity update, removi dto subject

---
+++ app/src/main/java/online/vidacademica/entities/SubjectDTO.java
@@ -0,0 +1,50 @@
+package online.vidacademica.entities;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+
+public class SubjectDTO implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private String name;
+    private boolean active;
+    private LocalDate startDate;
+    private LocalDate endDate;
+
+    public static long getSerialVersionUID() {
+        return serialVersionUID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDate endDate) {
+        this.endDate = endDate;
+    }
+}
