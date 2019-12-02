8916824b42e65e67f1ca63087979d62e5f63a4ab
Carlos <cgletras@gmail.com>
2019-12-02 04:57:46 -0300

    Associar alunos a classe. O layout esta pronto. Estou mexendo no adpter ainda.

---
+++ app/src/main/java/online/vidacademica/activity_create_registration.java
@@ -0,0 +1,14 @@
+package online.vidacademica;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_create_registration extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_registration);
+    }
+}
