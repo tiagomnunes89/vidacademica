48279b2b52f6b0eaf5560cce5017361c2067db66
Tiago M. Nunes <37198181+tiagomnunes89@users.noreply.github.com>
2019-12-06 18:44:54 -0300

    home student

--- app/src/main/java/online/vidacademica/view/ui/HomeActivity.java
+++ app/src/main/java/online/vidacademica/view/ui/HomeActivity.java
@@ -1,16 +1,28 @@
 package online.vidacademica.view.ui;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
 import online.vidacademica.R;
+import online.vidacademica.databinding.ActivityHomeBinding;
 
 public class HomeActivity extends AppCompatActivity {
+
+    private ActivityHomeBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_home);
+        binding.setLifecycleOwner(this);
+
+        binding.layoutContentBottomCards.cardViewMyNotes.setOnClickListener(v -> startActivity(
+                new Intent(this, MyScoresActivity.class)));
+
+        binding.layoutContentBottomCards.cardViewMySubjects.setOnClickListener(v -> startActivity(
+                new Intent(this, ListMySubjectsActivity.class)));
     }
 }
