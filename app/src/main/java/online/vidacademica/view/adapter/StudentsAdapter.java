e6d00e07142b94fabd4dddfbc82ca47d521cea8d
Carlos <cgletras@gmail.com>
2019-12-06 12:57:46 -0300

    Students adapter pronto. Não da erro mas a activity esta dando crash

--- app/src/main/java/online/vidacademica/view/adapter/StudentsAdapter.java
+++ app/src/main/java/online/vidacademica/view/adapter/StudentsAdapter.java
@@ -1,4 +1,60 @@
 package online.vidacademica.view.adapter;
 
-public class StudentsAdapter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import online.vidacademica.R;
+import online.vidacademica.entities.UserEntity;
+
+public class StudentsAdapter extends RecyclerView.Adapter<StudentsAdapter.StudentsViewHolder> {
+
+    List<UserEntity> students = new ArrayList<>();
+    LayoutInflater layoutInflater;
+
+    public StudentsAdapter (Context context, List<UserEntity> students){
+        this.students = students;
+        this.layoutInflater = LayoutInflater.from(context);
+    }
+
+    @NonNull
+    @Override
+    public StudentsAdapter.StudentsViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = layoutInflater.inflate(R.layout.item_recycler_student, parent, false);
+        return new StudentsViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull StudentsAdapter.StudentsViewHolder holder, int position) {
+        UserEntity student = students.get(position);
+        holder.StudentName.setText(student.getName());
+        holder.dateOfBirth.setText(student.getDateOfBirth());
+    }
+
+    @Override
+    public int getItemCount() {
+        return students.size();
+    }
+
+    public class StudentsViewHolder extends RecyclerView.ViewHolder {
+
+        TextView StudentName;
+        TextView dateOfBirth;
+        EditText EdtNome;
+
+        public StudentsViewHolder(@NonNull View itemView) {
+            super(itemView);
+            StudentName = itemView.findViewById(R.id.item_student_name);
+            dateOfBirth = itemView.findViewById(R.id.item_student_date_of_birth);
+        }
+    }
 }
--- app/src/main/java/online/vidacademica/view/ui/CreateRegistrationActivity.java
+++ app/src/main/java/online/vidacademica/view/ui/CreateRegistrationActivity.java
@@ -1,16 +1,53 @@
 package online.vidacademica.view.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.LinearLayoutCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import online.vidacademica.R;
+import online.vidacademica.entities.UserEntity;
+import online.vidacademica.view.adapter.StudentsAdapter;
 
 public class CreateRegistrationActivity extends AppCompatActivity {
+
+    List<UserEntity> students = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_registration);
+
+        getStudents();
+
+        startRecycler();
+    }
+
+    private void startRecycler() {
+        StudentsAdapter adapter = new StudentsAdapter(this, students);
+        RecyclerView recyclerView = findViewById(R.id.student_recycler);
+        recyclerView.setAdapter(adapter);
+
+        //Criar layout manager
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(linearLayoutManager);
+    }
+
+    private void getStudents() {
+        UserEntity stu1 = new UserEntity(null, "Carlos", "email", "1978-11-16", "bla", "bla");
+        UserEntity stu2 = new UserEntity(null, "Duduzera Comboy", "email", "1978-11-16", "bla", "bla");
+        UserEntity stu3 = new UserEntity(null, "Sotero Overflow", "email", "1978-11-16", "bla", "bla");
+        UserEntity stu4 = new UserEntity(null, "Tiaguera", "email", "1978-11-16", "bla", "bla");
+
+        students.add(stu1);
+        students.add(stu2);
+        students.add(stu3);
+        students.add(stu4);
     }
 }
