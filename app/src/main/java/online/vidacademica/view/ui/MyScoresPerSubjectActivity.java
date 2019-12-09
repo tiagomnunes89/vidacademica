67e95c341c9f4fedb746617eaaf0aac437b94088
Rafael Sotero Rocha <29471402+soterocra@users.noreply.github.com>
2019-12-09 01:18:35 -0300

    Passagem de geral para detalhe em adapter de scores funcionando corretamente

--- app/src/main/java/online/vidacademica/view/ui/MyScoresPerSubjectActivity.java
+++ app/src/main/java/online/vidacademica/view/ui/MyScoresPerSubjectActivity.java
@@ -12,7 +12,7 @@ import online.vidacademica.R;
 import online.vidacademica.entities.TestResultDTO;
 import online.vidacademica.view.adapter.MyScoresPerSubjectAdapter;
 
-public class MyScoresPerSubject extends AppCompatActivity {
+public class MyScoresPerSubjectActivity extends AppCompatActivity {
 
     List<TestResultDTO> testResultDTOList = new ArrayList<>();
 
